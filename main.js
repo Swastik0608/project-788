f591bc167d59002020dfd5b457786215122291b5
Swastik0608 <102349946+Swastik0608@users.noreply.github.com>
2022-12-18 22:18:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var images = [family_book.jpg,mummy.jpg, papa.jpg , sister.jpg, grandmother.jpg, grandfather.jpg];
+var names = ["Family Book","Deepika Gupta", "Gaurav Mittal", "Darshika Mittal", "Kalpana Mittal", "Krishna Kumar Mittal"];
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 5
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+      }
+    
+    //Debug the code to store list of images in updatedImage. Use images[i]
+    var updatedImage = [images];
+    //Debug the code to store list of names in updatedName. Use names[i]
+    var updatedName = [names] ;
+ 
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
